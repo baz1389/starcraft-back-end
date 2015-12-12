a72a3bbdc712bde9dabde68e8638c0bf0f8df11f
Ben <benjamin.a.spencer@gmail.com>
2015-12-12 16:22:26 -0500

    adds guide model to models/index.js and changes mongoose.connect to reference the guide mongo db

--- models/index.js
+++ models/index.js
@@ -4,7 +4,8 @@ var mongoose = require('mongoose');
 mongoose.Promise = Promise;
 
 mongoose.model('User', require('./User'));
+mongoose.model('Guide', require('./guide'));
 
-mongoose.connect("mongodb://localhost/passport-lesson");
+mongoose.connect("mongodb://localhost/guide");
 
 module.exports = mongoose;
