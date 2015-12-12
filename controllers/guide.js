76b76a4559752e2096cbbd96ad287d1070706915
Ben <benjamin.a.spencer@gmail.com>
2015-12-12 16:34:43 -0500

    adds template for guide controller

---
+++ controllers/guide.js
@@ -0,0 +1,14 @@
+
+var mongoose = require("../models/index");
+var passport = require('passport');
+
+var Guide = require('../models').model('Guide');
+var User = require('../models').model('User');
+
+var guideController = {
+  readAll: ,
+  readOne: ,
+  createGuide: ,
+  updateGuide: ,
+  destroyGuide:
+}
